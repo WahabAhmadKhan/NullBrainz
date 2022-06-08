59725fa6017a0449963428ffbfb590a825857194
Wahab Ahmad Khan <51134741+WahabAhmadKhan@users.noreply.github.com>
2022-06-08 23:46:00 +0500

    delete

--- src/components/imageSlider.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-class ImageSlide extends Component {
-    state = {  }
-    render() { 
-        return (  <></>);
-    }
-}
- 
-export default ImageSlide;
