815b6b9553288d816225eac46ed58b9370c4a359
Wahab Ahmad Khan <51134741+WahabAhmadKhan@users.noreply.github.com>
2022-06-09 10:25:07 +0500

    sqa

---
+++ companiesProfile.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html>
+
+<head>
+	<meta charset="utf-8">
+	<title>Hashmi Group Of Companies</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+	<meta name="apple-mobile-web-app-capable" content="yes">
+	<link href="css/default.css" rel="stylesheet" type="text/css">
+	<script src="js/jquery-3.5.1.min.js"></script>
+	<script src="js/owl-carousel.js"></script>
+</head>
+
+<body>
+
+<?php include('header.php') ?>
+	<div class="main-banner">
+		<div class="owl-carousel owl1">
+			<div class="item main-img" style="background-image: url(images/about-bnr.jpg);">
+				<a href="">
+					<header class="main-banner-header">
+						<div class="grid1170 center">
+							<div class="image-info playfair">ORGANOGRAM</div>
+						</div>
+					</header>
+				</a>
+			</div>
+		</div>
+	</div>
+
+	<div class="grid1260 f-zero pdm100 chrmn-speech  center">
+		<img src="images/organogram.png" alt="organogram" />
+	</div>
+	<?php include('footer.php') ?>
+
+</body>
+
+</html>
